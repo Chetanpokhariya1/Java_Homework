a78a9a07ca2dc16e34f34665160f5b362d999c9b
chetanpokhariya1 <pokhariyachetan90@gmail.com>
2023-10-29 15:12:19 +0530

    new

--- _01_29oct_hw/src/Factorial.java
+++ _01_29oct_hw/src/Factorial.java
@@ -1,9 +1,12 @@
 public class Factorial {
     public static void main(String[] args) {
         int num = 4;
-        int factorial = 0;
-        for (int i = 1; i<=num; i++) {
+        int i=1;
+        int factorial = 1;
+        for (int i = 1; i<=num-1; i++) {
+            factorial=num*i;
             factorial*=i;
+            System.out.println(factorial);
         }
         System.out.println(factorial);
     }
