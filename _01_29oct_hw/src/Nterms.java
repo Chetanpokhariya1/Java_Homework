a5ab689fbaeffcb9fa0563327a51d7f4e8e666ea
chetanpokhariya1 <pokhariyachetan90@gmail.com>
2023-10-29 13:37:42 +0530

    factorial

---
+++ _01_29oct_hw/src/Nterms.java
@@ -0,0 +1,9 @@
+public class Nterms {
+    public static void main(String[] args) {
+        int num=5;
+        int sum=0;
+        for (int i=1;i<=num;i++){
+            sum+=1;
+        }
+        System.out.println(sum);}
+}
